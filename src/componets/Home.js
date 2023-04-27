82dc53253fec6920aa32ff2b096c69881b624568
http.anish <koder2077@gmail.com>
2023-04-27 14:44:40 +0530

    ADD:Section for the Tesla

--- src/componets/Home.js
+++ src/componets/Home.js
@@ -1,12 +1,46 @@
 import React from 'react'
 import styled from 'styled-components'
+import Section from './Section'
 
 function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <Container>
+        <Section
+            title="Model S"
+            description="Order Online for Touchless Delivery"
+            backgroundImg="model-s.jpg"
+            leftBtnText="Custom order"
+            rightBtnText="Existing invenatory"
+        />
+        <Section
+            title="Model Y"
+            description="Order Online for Touchless Delivery"
+            backgroundImg="model-y.jpg"
+            leftBtnText="Custom order"
+            rightBtnText="Existing invenatory"
+        />
+        <Section
+           title="Model 3"
+           description="Order Online for Touchless Delivery"
+           backgroundImg="model-3.jpg"
+           leftBtnText="Custom order"
+           rightBtnText="Existing invenatory" 
+        />
+        <Section
+            title="Model X"
+            description="Order Online for Touchless Delivery"
+            backgroundImg="model-x.jpg"
+            leftBtnText="Custom order"
+            rightBtnText="Existing invenatory"
+        />
+        <Section
+            
+        />
+    </Container>
   )
 }
 
 export default Home
+const Container = styled.div`
+    height:100vh;
+`
